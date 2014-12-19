d1b08aaa0752b231ba3ff4d6ee1076d6d175544e
Adriano Emerick Cola <adrianocola@gmail.com>
2014-12-19 14:48:58 -0200

    Reverted secure thing from last commit and forced page to be http always (big hack)

--- index.js
+++ index.js
@@ -444,7 +444,7 @@ $(function(){
         var ladda = $("#create_room").ladda();
         ladda.ladda( 'start' );
 
-        peer = new Peer(makeid(4),{key: 'hi5939pursjaif6r', secure: true});
+        peer = new Peer(makeid(4),{key: 'hi5939pursjaif6r'});
 
         peer.on('open', function(id) {
             game_info.room = id;
@@ -494,7 +494,7 @@ $(function(){
 
     $("#enter_room").click(function(){
 
-        peer = new Peer(makeid(7),{key: 'hi5939pursjaif6r', secure: true});
+        peer = new Peer(makeid(7),{key: 'hi5939pursjaif6r'});
 
         $('.row.content').fadeOut();
         $("#room_controls").fadeOut(function(){
