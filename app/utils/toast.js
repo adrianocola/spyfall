0b870d1f43b193bcf8381baec69ce519b38b9915
Adriano Cola <adrianocola@gmail.com>
2019-06-09 23:47:36 -0300

    Changed toastr colors

--- app/utils/toast.js
+++ app/utils/toast.js
@@ -1,11 +1,14 @@
 import { toast } from 'react-toastify';
+import { css } from 'emotion';
 import i18n from 'i18n';
+import {COLORS} from 'styles/consts';
 
 export const showError = (msg, err) => {
   if(err) console.error(err); // eslint-disable-line no-console
   toast(i18n.t(msg || 'interface.error'), {
     position: toast.POSITION.TOP_CENTER,
     type: toast.TYPE.ERROR,
+    className: styles.error,
   });
 };
 
@@ -13,5 +16,15 @@ export const showSuccess = () => {
   toast(i18n.t('interface.done'), {
     position: toast.POSITION.TOP_CENTER,
     type: toast.TYPE.SUCCESS,
+    className: styles.success,
   });
+};
+
+const styles = {
+  error: css({
+    backgroundColor: COLORS.red,
+  }),
+  success: css({
+    backgroundColor: COLORS.green,
+  }),
 };
