b960b63e5840cf7e6065af5cc735847fa5cd21bd
Adriano Cola <adrianocola@gmail.com>
2019-06-10 00:49:04 -0300

    Added cron function to delete rooms and exports older than 7 days

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,25 @@
+const _ = require('lodash');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const moment = require('moment');
+const promiseLimit = require('promise-limit');
 
 admin.initializeApp();
-admin.firestore().settings({timestampsInSnapshots: true});
 
-exports.updatePlayer = functions.https.onCall((data, context) =>
-  admin.firestore().collection('rooms').doc(data.roomId).get().then((roomQuerySnapshot) => {
-    if (!roomQuerySnapshot.exists) {
-      return 'invalid_room';
-    }
+// once per day
+exports.deleteOld = functions.pubsub.schedule('0 0 * * *').onRun(async (context) => {
+  const limit = promiseLimit(10);
+  const oneWeekAgo = moment.utc().subtract(1, 'week').valueOf();
 
-    const roomData = roomQuerySnapshot.data();
+  const roomsSnapshot = await admin.database().ref('rooms').orderByChild('updatedAt').endAt(oneWeekAgo).once('value');
+  const rooms = roomsSnapshot.val();
+  await Promise.all(_.map(rooms, (v, roomId) =>
+    limit(() => admin.database().ref(`rooms/${roomId}`).remove())
+  ));
 
-    return admin.firestore().collection('rooms').doc(data.roomId).update({
-      playersStatus: {
-        ...roomData.playersStatus,
-        [data.playerName]: {
-          ...data.data,
-          updatedAt: new Date(),
-          userId: context.auth.uid,
-        },
-      },
-    }).then(() => true);
-  })
-);
+  const exportsSnapshot = await admin.database().ref('exports').orderByChild('createdAt').endAt(oneWeekAgo).once('value');
+  const exports = exportsSnapshot.val();
+  await Promise.all(_.map(exports, (v, exportId) =>
+    limit(() => admin.database().ref(`exports/${exportId}`).remove())
+  ));
+});
