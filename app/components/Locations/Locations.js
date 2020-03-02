379a5c5baed06e2ba30de2d2a0e72139eacda1ed
Samuelote <samote.wood@gmail.com>
2020-03-02 15:25:32 -0700

    added location cross-out functionality

--- app/components/Locations/Locations.js
+++ app/components/Locations/Locations.js
@@ -1,20 +1,28 @@
 import _ from 'lodash';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import ReactHtmlParser from 'react-html-parser';
-import {css} from 'emotion';
-import {DARK_COLORS, SHADES} from 'styles/consts';
+import { css } from 'emotion';
+import { DARK_COLORS, SHADES } from 'styles/consts';
 import { useTranslation } from 'react-i18next';
 import { DEFAULT_LOCATIONS } from 'consts';
 
-export const Locations = ({location, locations = {}, prevLocation}) => {
+export const Locations = ({ location, locations = {}, prevLocation }) => {
   const [t] = useTranslation();
+  const [sortedLocations, updateSortedLocations] = useState([]);
 
-  const sortedLocations = useMemo(() => {
+  useEffect(() => {
+    updateSortedLocations(initialLocations);
+  }, [initialLocations]);
+
+  const initialLocations = useMemo(() => {
     const locationsArray = _.map(locations, (locationObj, locationId) => ({
       ...locationObj,
-      name: DEFAULT_LOCATIONS[locationId] ? t(`location.${locationId}`) : locationObj.name,
+      name: DEFAULT_LOCATIONS[locationId]
+        ? t(`location.${locationId}`)
+        : locationObj.name,
       locationId,
+      crossOut: false,
     }));
     return _.orderBy(locationsArray, 'name');
   }, [locations, t]);
@@ -29,33 +37,68 @@ export const Locations = ({location, locations = {}, prevLocation}) => {
   const locationsLeft = useMemo(() => locationsChunks[0], [locationsChunks]);
   const locationsRight = useMemo(() => locationsChunks[1], [locationsChunks]);
 
+  const crossOutLocation = (selected) => {
+    const locationsCopy = [...sortedLocations]; // makes copy of locations
+    const index = locationsCopy.indexOf(selected); // gets index of selected location
+    const itemToUpdate = locationsCopy[index]; // selects location to update
+    itemToUpdate.crossOut = !itemToUpdate.crossOut; // sets bool to determine if crossed out
+    updateSortedLocations(locationsCopy); // updates are main sortedLocations array
+  };
+
   return (
     <Container>
       <Row>
         <Col xs={6}>
-          {locationsLeft.map((locationObj) =>
+          {locationsLeft.map((locationObj) => (
             <Row key={locationObj.locationId}>
               <Col>
-                <div className={`${styles.location} ${prevLocation === locationObj.locationId ? styles.prevLocation : ''} ${location === locationObj.locationId ? styles.highlight : ''}`}>
+                <div
+                  onClick={() => crossOutLocation(locationObj)}
+                  className={`${styles.location} 
+                    ${
+                    prevLocation === locationObj.locationId
+                      ? styles.prevLocation
+                      : ''
+                    } ${
+                    locationObj.crossOut && prevLocation !== undefined
+                      ? styles.crossedOut
+                      : ''
+                    } ${
+                    location === locationObj.locationId ? styles.highlight : ''
+                    }`}
+                >
                   {ReactHtmlParser(locationObj.name)}
                 </div>
               </Col>
             </Row>
-          )}
+          ))}
         </Col>
         <Col xs={6}>
-          {locationsRight.map((locationObj) =>
+          {locationsRight.map(locationObj => (
             <Row key={locationObj.locationId}>
               <Col>
-                <div className={`${styles.location} ${prevLocation === locationObj.locationId ? styles.prevLocation : ''} ${location === locationObj.locationId ? styles.highlight : ''}`}>
+                <div
+                  onClick={() => crossOutLocation(locationObj)}
+                  className={`${styles.location} 
+                    ${
+                    prevLocation === locationObj.locationId
+                      ? styles.prevLocation
+                      : ''
+                    } ${
+                    locationObj.crossOut && prevLocation !== undefined
+                      ? styles.crossedOut
+                      : ''
+                    } ${
+                    location === locationObj.locationId ? styles.highlight : ''
+                    }`}
+                >
                   {ReactHtmlParser(locationObj.name)}
                 </div>
               </Col>
             </Row>
-          )}
+          ))}
         </Col>
       </Row>
-
     </Container>
   );
 };
@@ -66,14 +109,20 @@ const styles = {
     textAlign: 'center',
     paddingLeft: 10,
     paddingRight: 10,
+    cursor: 'pointer',
+    userSelect: 'none',
   }),
   prevLocation: css({
+    textDecoration: 'line-through',
+    cursor: 'default',
+  }),
+  crossedOut: css({
     textDecoration: 'line-through',
   }),
   highlight: css({
     fontWeight: 'bold',
-    color: DARK_COLORS.red,
-  }),
+    color: DARK_COLORS.red
+  })
 };
 
 export default Locations;
