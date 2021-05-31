7bce3104c320656bfcc6bc65041226444f9a8460
Adriano Cola <adrianocola@gmail.com>
2021-05-31 11:45:21 -0300

    Removed config files

--- env/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  FIREBASE_API_KEY: 'AIzaSyCWJVCqcs1RgLKbf8SUSX4wc5w1mYUEVh4', // apiKey
-  FIREBASE_PROJECT_ID: 'spyfall-adriano-dev', // projectId
-  FIREBASE_SENDER_ID: '947322604050', // messagingSenderId
-  FIREBASE_APP_ID: '1:947322604050:web:3f5b0c1bc3d7fa89c366cb', // appId
-  FIREBASE_MEASUREMENT_ID: 'G-8EG7Z3D2KK', // measurementId
-  ADSENSE_CLIENT_ID: 'ca-pub-4238644655871928',
-};
